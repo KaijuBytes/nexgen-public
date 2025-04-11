c55b703756c1f3f955b276f823cef87d8566510e
KaijuBytes <lanz.martinez1205@gmail.com>
2025-04-11 07:34:03 +0000

    Support and Project Module updates

---
+++ nex/projects/report/project_summary/project_summary.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Project Summary"] = {
+	filters: [
+		{
+			fieldname: "is_active",
+			label: __("Is Active"),
+			fieldtype: "Select",
+			options: "\nYes\nNo",
+			default: "Yes",
+		},
+		{
+			fieldname: "status",
+			label: __("Status"),
+			fieldtype: "Select",
+			options: "\nOpen\nCompleted\nCancelled",
+			default: "Open",
+		},
+		{
+			fieldname: "project_type",
+			label: __("Project Type"),
+			fieldtype: "Link",
+			options: "Project Type",
+		},
+		{
+			fieldname: "priority",
+			label: __("Priority"),
+			fieldtype: "Select",
+			options: "\nLow\nMedium\nHigh",
+		},
+	],
+};
